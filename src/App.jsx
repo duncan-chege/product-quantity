1a78acdb0c194c997a11110fa42ce17298dae5f1
viisualworks <dshege4@gmail.com>
2025-01-08 13:27:34 +0300

    add images to the design

--- src/App.jsx
+++ src/App.jsx
@@ -46,66 +46,71 @@ function App() {
 
   return (
     <div
-      className="bg-cyan-700 min-h-screen flex justify-center items-center"
+      className="bg-[url('/assets/pampers-bg.jpg')] bg-no-repeat bg-cover bg-cyan-700 min-h-screen flex justify-center items-center"
       role="main">
-      <div className="bg-white rounded-md max-w-md w-1/2">
-        <h1 className="py-4 text-center font-bold text-2xl text-cyan-800">
-          Enter Values
-        </h1>
-        <form className="p-6" onSubmit={(e) => e.preventDefault()}>
-          <div>
-            <label className="text-cyan-800 font-medium" htmlFor="product">
-              Product Name
-            </label>
-            <br />
-            <input
-              className="border-2 border-solid border-slate-400 rounded mt-1 p-2 w-full"
-              type="text"
-              id="product"
-              value={product}
-              onChange={trackValues}
-              name="product"
-            />
-            {errors.product && (
-              <p className="text-sm text-red-600 pt-1">
-                Product name is needed
-              </p>
-            )}
-          </div>
-          <div className="my-4">
-            <label className="text-cyan-800 font-medium" htmlFor="quantity">
-              Quantity
-            </label>
-            <br />
-            <input
-              className="border-2 border-solid border-slate-400 rounded mt-1 p-2 w-full"
-              type="number"
-              id="quantity"
-              value={quantity}
-              onChange={trackValues}
-              name="quantity"
-            />
-            {errors.quantity && (
-              <p className="text-sm text-red-600 pt-1">
-                Quantity number is needed
-              </p>
-            )}
-          </div>
-          <div class="text-center">
-            <button
-              className={
-                errors.product || errors.quantity || !product || !quantity
-                  ? "px-4 bg-slate-400 text-white py-2 rounded"
-                  : "px-4 bg-cyan-600 text-white py-2 rounded"
-              }
-              type="submit"
-              disabled={
-                errors.product || errors.quantity || !product || !quantity
-              }>
-              Submit
-            </button>
-          </div>
-        </form>
+      <div className="bg-white rounded-lg max-w-3xl w-full grid grid-cols-2">
+        <div>
+          <h1 className="py-4 text-center font-bold text-2xl text-cyan-800">
+            Make Your Diaper Order
+          </h1>
+          <form className="p-6" onSubmit={(e) => e.preventDefault()}>
+            <div>
+              <label className="text-cyan-800 font-medium" htmlFor="product">
+                Product Name
+              </label>
+              <br />
+              <input
+                className="border-2 border-solid border-slate-400 rounded mt-1 p-2 w-full"
+                type="text"
+                id="product"
+                value={product}
+                onChange={trackValues}
+                name="product"
+              />
+              {errors.product && (
+                <p className="text-sm text-red-600 pt-1">
+                  Product name is needed
+                </p>
+              )}
+            </div>
+            <div className="my-4">
+              <label className="text-cyan-800 font-medium" htmlFor="quantity">
+                Quantity
+              </label>
+              <br />
+              <input
+                className="border-2 border-solid border-slate-400 rounded mt-1 p-2 w-full"
+                type="number"
+                id="quantity"
+                value={quantity}
+                onChange={trackValues}
+                name="quantity"
+              />
+              {errors.quantity && (
+                <p className="text-sm text-red-600 pt-1">
+                  Quantity number is needed
+                </p>
+              )}
+            </div>
+            <div className="text-center">
+              <button
+                className={
+                  errors.product || errors.quantity || !product || !quantity
+                    ? "px-4 bg-slate-400 text-white py-2 rounded"
+                    : "px-4 bg-cyan-600 text-white py-2 rounded"
+                }
+                type="submit"
+                disabled={
+                  errors.product || errors.quantity || !product || !quantity
+                }>
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <div>
+          <img className="rounded-r-lg" src="/product-quantity/assets/pampers.jpg" alt="Pampers" />
+        </div>
       </div>
     </div>
   );
